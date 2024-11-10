770033490ea450c185bc742724c3842822db98e6
trama00 <luca.tramacere@gmail.com>
2024-11-10 12:14:53 +0100

    commented keras_tuner

--- libraries.py
+++ libraries.py
@@ -7,7 +7,7 @@ import os
 import tensorflow as tf
 from tensorflow import keras as tfk
 from keras import layers as tfkl
-from keras_tuner import RandomSearch
+# from keras_tuner import RandomSearch
 from tensorflow.keras.applications import ResNet50
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix, roc_auc_score, classification_report
 from sklearn.preprocessing import label_binarize
