291f0f7c683d234d156b008eac96cd0f22acd76f
trama00 <luca.tramacere@gmail.com>
2024-11-13 00:09:45 +0100

    added base models

--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
 import numpy as np
 import tensorflow as tf
 from multiprocessing import Pool
-
+from tensorflow.keras.applications import (
+    Xception, VGG16, VGG19, ResNet50, ResNet50V2, ResNet101, ResNet101V2,
+    ResNet152, ResNet152V2, InceptionV3, InceptionResNetV2, MobileNet,
+    MobileNetV2, DenseNet121, DenseNet169, DenseNet201, NASNetMobile,
+    NASNetLarge, EfficientNetB0, EfficientNetB1, EfficientNetB2, EfficientNetB3,
+    EfficientNetB4, EfficientNetB5, EfficientNetB6, EfficientNetB7,
+    EfficientNetV2B0, EfficientNetV2B1, EfficientNetV2B2, EfficientNetV2B3,
+    EfficientNetV2S, EfficientNetV2M, EfficientNetV2L, ConvNeXtTiny,
+    ConvNeXtSmall, ConvNeXtBase, ConvNeXtLarge, ConvNeXtXLarge
+)
 # Function to check if an image matches the conditions
 def check_image(i, img, shrek, troll, tol):
     """Check if an image matches the shrek or troll images within a tolerance."""
@@ -20,3 +29,30 @@ def parallel_index_removal(data, shrek, troll, tol=0.0001, num_workers=4):
     index_to_remove = [index for index in results if index is not None]
     
     return index_to_remove
+
+
+
+def get_base_model(model_name, input_shape):
+    models = {
+        'Xception': Xception, 'VGG16': VGG16, 'VGG19': VGG19, 'ResNet50': ResNet50,
+        'ResNet50V2': ResNet50V2, 'ResNet101': ResNet101, 'ResNet101V2': ResNet101V2,
+        'ResNet152': ResNet152, 'ResNet152V2': ResNet152V2, 'InceptionV3': InceptionV3,
+        'InceptionResNetV2': InceptionResNetV2, 'MobileNet': MobileNet, 'MobileNetV2': MobileNetV2,
+        'DenseNet121': DenseNet121, 'DenseNet169': DenseNet169, 'DenseNet201': DenseNet201,
+        'NASNetMobile': NASNetMobile, 'NASNetLarge': NASNetLarge, 'EfficientNetB0': EfficientNetB0,
+        'EfficientNetB1': EfficientNetB1, 'EfficientNetB2': EfficientNetB2, 'EfficientNetB3': EfficientNetB3,
+        'EfficientNetB4': EfficientNetB4, 'EfficientNetB5': EfficientNetB5, 'EfficientNetB6': EfficientNetB6,
+        'EfficientNetB7': EfficientNetB7, 'EfficientNetV2B0': EfficientNetV2B0, 'EfficientNetV2B1': EfficientNetV2B1,
+        'EfficientNetV2B2': EfficientNetV2B2, 'EfficientNetV2B3': EfficientNetV2B3, 'EfficientNetV2S': EfficientNetV2S,
+        'EfficientNetV2M': EfficientNetV2M, 'EfficientNetV2L': EfficientNetV2L, 'ConvNeXtTiny': ConvNeXtTiny,
+        'ConvNeXtSmall': ConvNeXtSmall, 'ConvNeXtBase': ConvNeXtBase, 'ConvNeXtLarge': ConvNeXtLarge,
+        'ConvNeXtXLarge': ConvNeXtXLarge
+    }
+    
+    if model_name not in models:
+        raise ValueError(f"Model '{model_name}' is not available. Choose from: {list(models.keys())}")
+    
+    base_model = models[model_name](include_top=False, input_shape=input_shape, weights='imagenet')
+    base_model.trainable = False  # Freeze the base model
+
+    return base_model
